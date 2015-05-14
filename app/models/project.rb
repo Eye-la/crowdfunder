c48bf90cb98e0edb603be6796ed48821fef1c617
Sandy Lai <sandywslai@gmail.com>
2015-05-14 10:46:48 -0400

    Added tags and view to see all projects for a tag

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,5 @@ class Project < ActiveRecord::Base
   belongs_to :category
 
   accepts_nested_attributes_for :rewards
+  acts_as_taggable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
+  get 'tags/:tag', to: 'projects#index', as: :tag
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
