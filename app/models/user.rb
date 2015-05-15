b1b7fc5bfafb680c1a4d6c4ad936c051815a86e3
Melissa D'Agruma <mdagruma@gmail.com>
2015-05-15 03:48:24 -0400

    created project_comment model for project comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   has_many :pledges
   has_many :rewards, through: :pledges
   has_many :backed_projects, through: :rewards, source: :project
+  has_many :project_comments
+  has_many :projects, through: :project_comments
 
 
   validates :password, length: { minimum: 5 }
